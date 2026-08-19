c2b76d178ff66b75227facc3cd863c4e3fd00e87
flip <flip.400g@gmail.com>
2026-08-19 17:40:32 -0300

    ultimo commit

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,24 +1,67 @@
-import "./Login.css";
-import { useNavigate } from "react-router-dom";
+import './Login.css'
+import { useState } from "react";
+import { useNavigate } from 'react-router-dom'
 
-function Login() {
-  const navegate = useNavigate()
-  function entrar(){
-    navegate("/home")
-  }
+function Login(){
+    
+    const navigate = useNavigate()
 
-    return (
-      <>
-        <div className="container">
-          <div className="card">
-          <h2>Login</h2>
-          <input id="input_usuario" type="text" placeholder="E-mail" />
-          <input id="input_senha" type="password" placeholder="Senha" />
-          <button onClick={entrar}>Entrar</button>
-        </div>
-        </div>
-      </>
-    );
-  }
-  
-  export default Login;
+    const [usuario, setUsuario] = useState("");//guarda o que foi digitado
+    const [senha, setSenha] = useState("");//guarda a senha
+    const [mensagem, setMensagem] = useState("");//mostra o resultado da validação
+
+    function entrar (){
+        if (usuario === "admin" && senha === "123") {
+
+        localStorage.setItem("usuario", usuario);
+        localStorage.setItem("perfil", "cozinha");
+
+        navigate("/pedido");
+
+    } 
+    
+    else if (usuario === "cliente" && senha === "123") {
+
+        localStorage.setItem("usuario", usuario);
+        localStorage.setItem("perfil", "cliente");
+
+        navigate("/home");
+    
+    }else {
+
+        setMensagem("Usuário ou senha inválidos.");
+
+    }
+    }
+
+    
+
+    return (
+        <>
+        <section className="login-container">
+            <h2>Login</h2>
+                <input type="text"  
+                placeholder='Usuário' 
+                value={usuario}
+                onChange={(e) => setUsuario(e.target.value)}/>
+                
+                <input 
+                type="password"
+                placeholder='Senha'
+                value={senha}
+                onChange={(e) => setSenha(e.target.value)}/>
+            <button onClick={entrar}>Enviar</button>
+            <p>{mensagem}</p>
+            
+            <p>Esqueceu a senha?</p>
+            
+
+        </section>
+        
+        
+        </>
+        
+    )
+}
+
+export default Login
